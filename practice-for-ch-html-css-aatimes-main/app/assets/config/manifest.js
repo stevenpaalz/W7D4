454dd7907bafb04624309d80647e9726ab5d705f
Shan Kim <shankim35@gmail.com>
2023-02-23 14:59:54 -0500

    phase 3

--- practice-for-ch-html-css-aatimes-main/app/assets/config/manifest.js
+++
@@ -1,4 +0,0 @@
-//= link_tree ../images
-//= link_directory ../builds .css
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
--- practice-for-ch-html-css-aatimes-main/app/javascript/controllers/modal_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-export default class extends Controller {
-  toggleModal () {
-    document.getElementById("search-modal").classList.toggle('expand');
-    document.getElementById("overlay").classList.toggle('hidden');
-  }
-}
